0874eadeb2060907d513d0efba20df1fa8a532a3
Ajmal Haris <ajmalharis678@gmail.com>
2021-10-12 13:09:51 +0530

    added few components to the task tracker application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [ tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'task of 1',
+        day: "Monday",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "task of 2",
+        day: "Tuesday",
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: "task of 3",
+        day: "Friday",
+        reminder: true,
+    }
+]);
+
   return (
     <div className="container">
       <Header title={"Task Tracker App"} />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,15 @@
-const tasks = [
-    {
-        id: 1,
-        text: "task of 1",
-        day: "Monday",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "task of 2",
-        day: "Tuesday",
-        reminder: false,
-    },
-    {
-        id: 1,
-        text: "task of 3",
-        day: "Friday",
-        reminder: true
-    }
-];
+import Task from "./Task";
 
-const Tasks = () => {
-
+const Tasks = ({ tasks }) => {
+    
+    return (
+    <>
+    {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+    ))}
+    
+    </>
+    )
 }
 
 export default Tasks;
