07a5a1f82bb5e19f07f695288550410e985bae34
Ajmal Haris <ajmalharis678@gmail.com>
2021-10-12 11:50:39 +0530

    initial commit of task tracker v2

---
+++ src/components/Tasks.js
@@ -0,0 +1,26 @@
+const tasks = [
+    {
+        id: 1,
+        text: "task of 1",
+        day: "Monday",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "task of 2",
+        day: "Tuesday",
+        reminder: false,
+    },
+    {
+        id: 1,
+        text: "task of 3",
+        day: "Friday",
+        reminder: true
+    }
+];
+
+const Tasks = () => {
+
+}
+
+export default Tasks;
